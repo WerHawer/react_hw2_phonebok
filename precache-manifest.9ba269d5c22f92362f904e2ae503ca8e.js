fdd13387f422c013bb213dec519054ab24d221d9
WerHawer <werhawer@gmail.com>
2020-04-15 22:38:13 +0300

    Updates

---
+++ precache-manifest.9ba269d5c22f92362f904e2ae503ca8e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a4cc511af3eb7caeaf2dbbe68e2fb1d",
+    "url": "/react_hw2_phonebok/index.html"
+  },
+  {
+    "revision": "2fb96199da8b320d43ba",
+    "url": "/react_hw2_phonebok/static/css/main.e1d131a2.chunk.css"
+  },
+  {
+    "revision": "3f9d6a9bc5541f35e0ee",
+    "url": "/react_hw2_phonebok/static/js/2.76657afc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react_hw2_phonebok/static/js/2.76657afc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2fb96199da8b320d43ba",
+    "url": "/react_hw2_phonebok/static/js/main.930c3874.chunk.js"
+  },
+  {
+    "revision": "cc044139f21d3ea0798d",
+    "url": "/react_hw2_phonebok/static/js/runtime-main.00f4d59c.js"
+  }
+]);
